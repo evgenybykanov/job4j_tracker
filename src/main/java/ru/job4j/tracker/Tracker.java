12232d1c4d6c1f3c87824b86aa2a4a5ab58cf817
evgenybykanov <red_9894@mail.ru>
2020-08-07 16:47:51 +0300

    Метод замены заявки. Tracker.replace [#310434]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -38,6 +31,33 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(result, number);
+    }
+
+
+    public boolean replace(int id, Item item) {
+        boolean result = false;
+        int index = indexOf(id);
+        for (; index < size; index++) {
+            if (items[index].equals(id)) {
+                this.items.add(index, item);
+                result = true;
+                item.setId(id);
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
     }
 
 }
