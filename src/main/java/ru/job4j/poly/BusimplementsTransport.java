ccb2e4f1e3efb1b9a8c644917a0f0c713a44cdd8
evgenybykanov <red_9894@mail.ru>
2020-08-19 15:35:24 +0300

    Интерфейс Input [#310366]

---
+++ src/main/java/ru/job4j/poly/BusimplementsTransport.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passebgers(int count) {
+
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        return 0;
+    }
+}
