f297171095b1aec35747cacc15fe2c1f1a40e73b
evgenybykanov <red_9894@mail.ru>
2020-08-21 14:19:56 +0300

    Шаблон проектирования - Стратегия [#310377]

--- src/main/java/ru/job4j/stragery/Square.java
+++ src/main/java/ru/job4j/stragery/Square.java
@@ -1,4 +1,15 @@
 package ru.job4j.stragery;
 
-public class Square {
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    " --------------" + ln
+                + " |            |" + ln
+                + " | ___________|" + ln;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Triangle().draw());
+    }
 }
