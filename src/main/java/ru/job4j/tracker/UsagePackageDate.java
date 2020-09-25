237f2d6293f71f588a866a1adebd10b1460b6b7c
evgenybykanov <red_9894@mail.ru>
2020-09-25 09:56:57 +0300

    Date. Отображение даты.

---
+++ src/main/java/ru/job4j/tracker/UsagePackageDate.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class UsagePackageDate {
+    public static void main(String[] args) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = currentDateTime.format(formatter);
+        System.out.println("Текущие дата и время   " + currentDateTimeFormat);
+    }
+}
+
