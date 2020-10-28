84c655cb70a8fe569f68d019483d3d7b4a1c832c
evgenybykanov <red_9894@mail.ru>
2020-10-28 14:33:06 +0300

    Замена if-else-throw на if-throw

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -11,31 +11,41 @@ public class ValidateInputTest {
     public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"one", "1"}
+                new String[]{"one", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
         assertThat(selected, is(1));
     }
+
     @Test
     public void whenCorrectInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0", "1"}
-        );
-        ValidateInput input = new ValidateInput(out, in);
-        int selected = input.askInt("Enter menu:");
-        assertThat(selected, is(0));
-    }
-    @Test
-    public void whenCorrectOutput() {
-        Output out = new StubOutput();
-        Input in = new StubInput(
-                new String[] {}
+                new String[]{"0", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
         assertThat(selected, is(0));
     }
 
+    @Override
+    public String toString() {
+        return "ValidateInputTest{}";
+    }
+
+    @Test
+    public void whenCorrectOutput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(out.toString(), is(
+                String.format(
+                        "Please enter validate data again.%n"
+                )
+        ));
+    }
 }
