aa85d3787e0c5c81a146a01a1b696efaa8fa6cac
evgenybykanov <red_9894@mail.ru>
2020-10-28 12:11:06 +0300

    Иерархия исключений и множественный catch

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -2,18 +2,31 @@ package ru.job4j.ex;
 
 public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        return null;
+        User result = null;
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                result = user;
+                break;
+            }
+        }
+        if (result == null) {
+            throw new UserNotFoundException("User not found.");
+        }
+        return result;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        return false;
+        if (user.getUsername().length() < 3 || !user.isValid()) {
+            throw new UserInvalidException("User is not valid.");
+        }
+        return true;
     }
 
     public static void main(String[] args) throws UserNotFoundException {
         User[] users = {
-                new User("Petr Arsentev", true)
+                new User("Evgeny Bykanov", true)
         };
-        User user = findUser(users, "Petr Arsentev");
+        User user = findUser(users, "Evgeny Bykanov");
         if (validate(user)) {
             System.out.println("This user has an access");
         }
