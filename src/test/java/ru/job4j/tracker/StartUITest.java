8c4996d8cf02e68f71b91b0c3ff00747869f1e07
evgenybykanov <red_9894@mail.ru>
2020-10-28 12:57:44 +0300

    Тесты на StartUI.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -108,6 +108,7 @@ public class StartUITest {
                         "1. === Exit menu ====" + System.lineSeparator()
         ));
     }
+
     @Test
     public void whenFindAllAction() {
         Output out = new StubOutput();
@@ -131,9 +132,6 @@ public class StartUITest {
         ));
     }
 
-
-
-
     @Test
     public void whenExit() {
         Output out = new StubOutput();
@@ -147,6 +145,28 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
                 "Menu." + System.lineSeparator() + "0. === Exit menu ====" + System.lineSeparator()
+        ));
+    }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "7"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. === Exit menu ====%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. 0. === Exit menu ====%n"
+                )
         ));
     }
 
